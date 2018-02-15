47ced6ab6a813355228f1ccc4fef9c5441e79645
Crone1 <34443838+Crone1@users.noreply.github.com>
2018-02-15 13:43:40 +0000

    Add files via upload

---
+++ Year One - Semester One/Week Two/square-perimeter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+side=input()
+print (4*side)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Year One - Semester One/Week Two/swap-two-lines.py
@@ -0,0 +1,17 @@
+sentance_one=raw_input()
+sentance_two=raw_input()
+
+print sentance_two
+print sentance_one
+
+
+
+
+
+
+
+
+
+
+
+
