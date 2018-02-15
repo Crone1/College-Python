8e8a25b1fdc485bc33240585dbc698f68f6aa404
Crone1 <34443838+Crone1@users.noreply.github.com>
2018-02-15 15:21:05 +0000

    Create backup-files-2.py

---
+++ Year One - Semester One/Week Nine/backup-files-2.py
@@ -0,0 +1,14 @@
+
+import os
+files = os.listdir('.')
+
+i = 0
+while i < len(files):
+   if files[i][0] != '.' and os.path.isfile(files[i]) and files[i][len(files[i])-4:] != '.bak':
+      copy = files[i] + '.bak'
+      with open(copy, 'w') as backup:
+         with open(files[i], 'r') as file:
+            text = file.read()
+            backup.write(text)
+
+   i = i + 1
