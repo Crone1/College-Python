17b61360e5a48f05e6d2fdaa878b3b5612e053a3
Crone1 <34443838+Crone1@users.noreply.github.com>
2018-02-15 13:57:38 +0000

    Add files via upload

---
+++ Year One - Semester One/Week Two/next-hailstone.py
@@ -0,0 +1,10 @@
+#!usr/bin/env python
+
+n=input()
+
+if n%2==0:
+   print n/2
+
+elif n%2==1:
+   print (n*3)+1
+
---
+++ Year One - Semester One/Week Two/weekend.py
@@ -0,0 +1,8 @@
+#!usr/bin/env python
+
+x=input()
+
+if 5<=x<=6:
+   print 'weekend'
+elif 4>=x>=0:
+   print 'weekday'
